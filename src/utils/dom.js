1a33dd840e4e0649d024fca13140755d138bdc66
shonie <astarnikov@thesolid.studio>
2018-12-15 19:05:57 +0200

    Removed unused modules

--- src/utils/dom.js
+++
@@ -1,9 +0,0 @@
-export const createNode = (type, attributes = {}, elementCreationOptions = {}) => {
-  const node = document.createElement(type, elementCreationOptions);
-
-  for (const attribute of Object.keys(attributes)) {
-    node.setAttribute(attribute, attributes[attribute]);
-  }
-
-  return node;
-};
